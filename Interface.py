0fc5fd5be52970d16ee963639ac6d1547b81a6d3
Jivitesh Rai <jiviteshrai17@gmail.com>
2023-03-23 18:13:46 +0530

    Final Changes to Interface

--- Interface.py
+++ Interface.py
@@ -14,14 +14,28 @@ class GUI(tk.Tk):
     def __init__(self):
         super(GUI, self).__init__()
         self.title("Voice Assistant")
-        self.geometry('500x500')
-        ttk.Button(self,text='SPEAK',command=self.run_va).place(x=200,y=200)
-        self.l=tk.Label(self,text='',borderwidth=1,relief='solid',height=8,width=28,anchor=tk.NW)
-        self.l.place(x=150,y=250)
+        self.geometry('700x700')
+        self.config(padx=40, pady=40, bg="#4F98CA")
+
+        self.va_img = tk.PhotoImage(file="va_icon.jpg")
+        self.label1 = tk.Label(self, image=self.va_img, bg="#4F98CA")
+        self.label1.place(x=180, y=0)
+        self.mic = tk.PhotoImage(file="mic.png")
+        self.speak = tk.Button(self, image=self.mic, highlightthickness=0, command=self.run_va, bg="#4F98CA")
+        self.speak.place(x=260, y=520)
+        self.label2 = tk.Label(self, bg="#4F98CA", fg="#37306B", text="Speak", font=("Courier", 24, "bold"))
+        self.label2.place(x=240, y=590)
 
     def run_va(self):
+
+        text = "Hello, try saying the following:\n\n  music\n  email\n  akinator\n  open youtube\n  where is (any " \
+               "location)"
+        self.label3 = tk.Label(self, bg="#4F98CA", fg="#F5EAEA", text=text, font=("Courier", 18, "bold"), justify="left")
+        self.label3.place(x=100, y=310)
+
         va=RunVA()
         va.start()
+
 
 if __name__=="__main__":
     gui=GUI()
--- MainVA/VA.py
+++ MainVA/VA.py
@@ -18,7 +18,7 @@ from akinator import *
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[2].id)
+engine.setProperty('voice', voices[1].id)
 
 
 
